d7221b48874f9799a9ff8bfe6545d54539433c63
sirati <git@sirati.me>
2026-09-22 12:51:51 +0200

    Remove obsolete CUDA maintainers cache

--- configuration-nixct-nvidia.nix
+++ configuration-nixct-nvidia.nix
@@ -44,7 +44,6 @@
       "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
       "https://cache.nixos-cuda.org"
-      "https://cuda-maintainers.cachix.org"
       "https://ai.cachix.org"
       "https://cache.flox.dev"
     ];
@@ -52,7 +51,6 @@
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
       "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
     ];
